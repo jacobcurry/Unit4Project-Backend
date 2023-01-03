6ff7324b6a47b11095b61344168c23a15bc110a6
QGonz95 <q.gonz95@gmail.com>
2023-01-03 16:03:41 -0500

    Updated serializer/model/urls

--- auth_api/serializers.py
+++ auth_api/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers 
 from .models import User 
+from django.contrib.auth.hashers import make_password, check_password
 
 class UserSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = User # tell django which model to use
-        fields = ('id', 'firstname', 'lastname', 'username', 'password',) # tell django which fields to include
+        fields = ('id', 'firstname', 'lastname', 'email', 'username', 'password',) # tell django which fields to include
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            firstname=validated_data['firstname'],
+            lastname=validated_data['lastname'],
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password= make_password(validated_data['password'])
+        )
+        user.save()
+        return user
+    
+    def update(self, instance, validated_data):
+        user = User.objects.get(email=validated_data["email"])
+        user.password = make_password(validated_data["password"])
+        user.save()
+        return user
--- auth_api/views.py
+++ auth_api/views.py
@@ -3,6 +3,10 @@ from rest_framework import generics
 from .serializers import UserSerializer
 from .models import User
 
+from django.contrib.auth.hashers import make_password, check_password
+from django.http import JsonResponse
+import json
+
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all().order_by('id') # tell django how to retrieve all objects from the DB, order by id ascending
     serializer_class = UserSerializer # tell django what serializer to use
@@ -10,3 +14,20 @@ class UserList(generics.ListCreateAPIView):
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
+
+def check_login(request):
+    if request.method=='Get':
+        return JsonResponse({})
+
+    if request.method=='PUT':
+        JsonRequest = json.loads(request.body)
+        email = jsonRequest['email']
+        password = JsonRequest['password']
+        if User.objects.get(email=email):
+            user = User.objects.get(email=email)
+            if check_password(password, user.password):
+                return JsonResponse({'id': user.id, 'email': user.email})
+            else:
+                return JsonResponse({})
+        else:
+            return JsonResponse({})
