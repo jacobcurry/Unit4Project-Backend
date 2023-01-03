0fc018fafd06644edd880b27e38a0778e31ccc0f
Kevin Souza <kevinxsouza@gmail.com>
2023-01-03 11:09:51 -0500

    posts backend basics done

--- posts_api/views.py
+++ posts_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .serializers import PostSerializer
+from .models import Post
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all().order_by('id')
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all().order_by('id')
+    serializer_class = PostSerializer
