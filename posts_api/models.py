67c15300805235e2f9a4e1e429ec280813d364f0
jacobcurry <jacob.a.curry@gmail.com>
2023-01-04 13:45:22 -0500

    fixed user model error

--- posts_api/models.py
+++ posts_api/models.py
@@ -3,7 +3,7 @@ from auth_api.models import User
 
 # Create your models here.
 class Post(models.Model):
-    user = models.ForeignKey(User.id, on_delete=models.CASCADE,)
+    user = models.ForeignKey(User.email, on_delete=models.CASCADE,)
     title = models.CharField(max_length=255)
     formBody = models.TextField(null=True, blank=True)
     imageURL = models.CharField(max_length=200, null=True, blank=True)
